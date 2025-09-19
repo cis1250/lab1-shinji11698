294c3febe163a6a8da76c47b01fe232d4baf6977
Chenuki Rodrigo <crodrigo@uoguelph.ca>
2025-09-19 11:35:31 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,4 +6,5 @@
 # 2. Add another print statement with your name.
 # 3. Add another print statement with your favorite hobby.
 
-print("Hello, world!")
+print("Chenuki Rodrigo")
+print("My hobby is playing video games")
